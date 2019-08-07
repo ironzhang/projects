7dfa46e295b7b360c028a568fac0a17b69bc2dc7
zhanghui <iron.zhang@foxmail.com>
2019-08-07 18:37:50 +0800

    add grpc helloworld

---
+++ testsrc/grpc/helloworld/client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ironzhang/projects/testsrc/grpc/protobuf/greet"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	const addr = "localhost:50051"
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to dial to %s: %v", addr, err)
+	}
+	defer conn.Close()
+	c := greet.NewGreeterClient(conn)
+
+	name := "world"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.SayHello(ctx, &greet.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("failed to say hello: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Mesg)
+}
---
+++ testsrc/grpc/helloworld/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/ironzhang/projects/testsrc/grpc/protobuf/greet"
+	"google.golang.org/grpc"
+)
+
+type Greeter struct{}
+
+func (p *Greeter) SayHello(ctx context.Context, req *greet.HelloRequest) (*greet.HelloReply, error) {
+	log.Printf("Received: %v", req.Name)
+	return &greet.HelloReply{Mesg: "Hello " + req.Name}, nil
+}
+
+func main() {
+	const addr = ":50051"
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	greet.RegisterGreeterServer(s, &Greeter{})
+
+	log.Printf("grpc serve on %v", addr)
+	if err = s.Serve(ln); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
