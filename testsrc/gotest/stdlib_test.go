5e569db3da1899542b8dcc039de45e017c8de4ca
zhanghui <iron.zhang@foxmail.com>
2019-10-24 22:44:22 +0800

    update

--- testsrc/gotest/stdlib_test.go
+++ testsrc/gotest/stdlib_test.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"testing"
 )
 
 func TestRelovse(t *testing.T) {
 	//net.LookupNS()
+}
+
+func TestPrintContext(t *testing.T) {
+	ctx0 := context.Background()
+	ctx1 := context.WithValue(ctx0, "key", "value")
+	fmt.Printf("%v\n", ctx0)
+	fmt.Printf("%v\n", ctx1)
 }
