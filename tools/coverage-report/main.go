8130ec0b496ceb7e9669824186dee5f8adf79d80
zhanghui <iron.zhang@foxmail.com>
2020-08-27 10:58:16 +0800

    update

--- tools/coverage-report/main.go
+++ tools/coverage-report/main.go
@@ -19,25 +19,95 @@ func main() {
 	r := bufio.NewReader(os.Stdin)
 
 	// 一级服务覆盖率
-	firsts, err := ReadCoverages(r)
+	firsts, err := ReadCoverageInfos(r)
 	if err != nil {
 		log.Printf("read coverages: %v", err)
 		return
 	}
 
 	// 核心服务覆盖率
-	cores, err := ReadCoverages(r)
+	cores, err := ReadCoverageInfos(r)
 	if err != nil {
 		log.Printf("read coverages: %v", err)
 		return
 	}
 
+	// 构造业务线信息
+	infos := MakeBusinessInfos(firsts, cores)
+
 	// 输出报告
-	err = WriteReport(os.Stdout, kind, firsts, cores)
+	err = WriteReport(os.Stdout, kind, infos)
 	if err != nil {
 		log.Printf("write report: %v", err)
 		return
 	}
+}
+
+type CoverageInfo struct {
+	TotalNum   int     // 服务总数
+	UsedNum    int     // 已使用服务发现的服务数量
+	Percentage float64 // 使用服务发现的服务占比
+}
+
+type BusinessInfo struct {
+	Name       string       // 业务线
+	Owner      string       // 负责人
+	FirstLevel CoverageInfo // 一级服务
+	CoreLevel  CoverageInfo // 核心服务
+}
+
+func StrToInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func StrToFloat64(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func ReadFields(r *bufio.Reader) ([]string, error) {
+	line, _, err := r.ReadLine()
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(line), "\t"), nil
+}
+
+func ReadCoverageInfos(r *bufio.Reader) (map[string]CoverageInfo, error) {
+	names, err := ReadFields(r)
+	if err != nil {
+		return nil, err
+	}
+	totals, err := ReadFields(r)
+	if err != nil {
+		return nil, err
+	}
+	useds, err := ReadFields(r)
+	if err != nil {
+		return nil, err
+	}
+	percentages, err := ReadFields(r)
+	if err != nil {
+		return nil, err
+	}
+
+	n := len(names)
+	m := make(map[string]CoverageInfo, n)
+	for i := 0; i < n; i++ {
+		m[names[i]] = CoverageInfo{
+			TotalNum:   StrToInt(totals[i]),
+			UsedNum:    StrToInt(useds[i]),
+			Percentage: StrToFloat64(percentages[i]),
+		}
+	}
+	return m, nil
 }
 
 func ReadCoverages(r *bufio.Reader) (map[string]string, error) {
@@ -62,23 +132,78 @@ func ReadCoverages(r *bufio.Reader) (map[string]string, error) {
 	return m, nil
 }
 
+func MakeBusinessInfos(firsts, cores map[string]CoverageInfo) []BusinessInfo {
+	var orders = []string{"地图", "引擎", "国际化", "2轮车", "车服", "安全", "业务中台", "业务平台", "顺风车", "金融", "代驾", "企业", "汇总"}
+	var owners = map[string]string{
+		"地图":   "黄楚宏",
+		"引擎":   "张慧",
+		"车服":   "陈洁",
+		"安全":   "吴迪",
+		"业务中台": "曹智轶",
+		"顺风车":  "张慧",
+		"金融":   "吴迪",
+		"企业":   "曹智轶",
+		"汇总":   "张慧",
+	}
+
+	infos := make([]BusinessInfo, 0, len(orders))
+	for _, name := range orders {
+		infos = append(infos, BusinessInfo{
+			Name:       name,
+			Owner:      owners[name],
+			FirstLevel: firsts[name],
+			CoreLevel:  cores[name],
+		})
+	}
+	return infos
+}
+
 var HTML = `<table border="1" cellborder="0" cellspacing="0" cellpadding="4">
 	<TR>
 	<TD>业务线</TD><TD>覆盖率</TD><TD>一级服务覆盖率</TD><TD>owner</TD>
 	</TR>
 	{{range .}}
 	<TR>
-	<TD>{{.Business}}</TD>
-	{{if lt .CoreCoverage 85.0}}
-		<TD><font color="red">{{.CoreCoverage}}</font></TD>
+	<TD>{{.Name}}</TD>
+	{{if lt .CoreLevel.Percentage 85.0}}
+		<TD><font color="red">{{.CoreLevel.Percentage}}</font></TD>
 	{{else}}
-		<TD>{{.CoreCoverage}}</TD>
+		<TD>{{.CoreLevel.Percentage}}</TD>
 	{{end}}
 
-	{{if lt .FirstCoverage 95.0}}
-		<TD><font color="red">{{.FirstCoverage}}</font></TD>
+	{{if lt .FirstLevel.Percentage 95.0}}
+		<TD><font color="red">{{.FirstLevel.Percentage}}</font></TD>
 	{{else}}
-		<TD>{{.FirstCoverage}}</TD>
+		<TD>{{.FirstLevel.Percentage}}</TD>
+	{{end}}
+
+	<TD>{{.Owner}}</TD>
+	</TR>
+	{{end}}
+</table>
+`
+
+var XHTML = `<table border="1" cellborder="0" cellspacing="0" cellpadding="4">
+	<TR>
+	<TD>业务线</TD><TD>核心服务数量</TD><TD>核心服务具备服务发现能力的服务数量</TD><TD>核心服务覆盖率</TD><TD>一级服务数量</TD><TD>一级服务具备服务发现能力的服务数量</TD><TD>一级服务覆盖率</TD><TD>owner</TD>
+	</TR>
+	{{range .}}
+	<TR>
+	<TD>{{.Name}}</TD>
+	<TD>{{.CoreLevel.TotalNum}}</TD>
+	<TD>{{.CoreLevel.UsedNum}}</TD>
+	{{if lt .CoreLevel.Percentage 85.0}}
+		<TD><font color="red">{{.CoreLevel.Percentage}}</font></TD>
+	{{else}}
+		<TD>{{.CoreLevel.Percentage}}</TD>
+	{{end}}
+
+	<TD>{{.FirstLevel.TotalNum}}</TD>
+	<TD>{{.FirstLevel.UsedNum}}</TD>
+	{{if lt .FirstLevel.Percentage 95.0}}
+		<TD><font color="red">{{.FirstLevel.Percentage}}</font></TD>
+	{{else}}
+		<TD>{{.FirstLevel.Percentage}}</TD>
 	{{end}}
 
 	<TD>{{.Owner}}</TD>
@@ -89,57 +214,27 @@ var HTML = `<table border="1" cellborder="0" cellspacing="0" cellpadding="4">
 
 var MD = `|业务线|覆盖率|一级服务覆盖率|owner|
 |------|------|--------------|-----|
-{{range .}}|{{.Business}}|{{if lt .CoreCoverage 85.0}} <font color="red">{{.CoreCoverage}}</font> {{else}} {{.CoreCoverage}} {{end}}|{{if lt .FirstCoverage 95.0}} <font color="red">{{.FirstCoverage}}</font> {{else}} {{.FirstCoverage}} {{end}}|{{.Owner}}|
+{{range .}}|{{.Name}}|{{if lt .CoreLevel.Percentage 85.0}} <font color="red">{{.CoreLevel.Percentage}}</font> {{else}} {{.CoreLevel.Percentage}} {{end}}|{{if lt .FirstLevel.Percentage 95.0}} <font color="red">{{.FirstLevel.Percentage}}</font> {{else}} {{.FirstLevel.Percentage}} {{end}}|{{.Owner}}|
 {{end}}
 `
 
-type Element struct {
-	Business      string
-	CoreCoverage  float64
-	FirstCoverage float64
-	Owner         string
-}
+var XMD = `|业务线|核心服务数量|核心服务具备服务发现能力的服务数量|核心服务覆盖率|一级服务数量|一级服务具备服务发现能力的服务数量|一级服务覆盖率|owner|
+|------|------------|----------------------------------|--------------|------------|----------------------------------|--------------|-----|
+{{range .}}|{{.Name}}|{{.CoreLevel.TotalNum}}|{{.CoreLevel.UsedNum}}|{{if lt .CoreLevel.Percentage 85.0}} <font color="red">{{.CoreLevel.Percentage}}</font> {{else}} {{.CoreLevel.Percentage}} {{end}}|{{.FirstLevel.TotalNum}}|{{.FirstLevel.UsedNum}}|{{if lt .FirstLevel.Percentage 95.0}} <font color="red">{{.FirstLevel.Percentage}}</font> {{else}} {{.FirstLevel.Percentage}} {{end}}|{{.Owner}}|
+{{end}}
+`
 
-func WriteReport(w io.Writer, kind string, firsts map[string]string, cores map[string]string) error {
-	var orders = []string{"地图", "引擎", "国际化", "2轮车", "车服", "安全", "业务中台", "业务平台", "顺风车", "金融", "代驾", "企业", "汇总"}
-	var owners = map[string]string{
-		"地图":   "黄楚宏",
-		"引擎":   "张慧",
-		"车服":   "陈洁",
-		"安全":   "吴迪",
-		"业务中台": "吴迪",
-		"顺风车":  "张慧",
-		"金融":   "吴迪",
-		"企业":   "吴迪",
-		"汇总":   "张慧",
-	}
-
-	elements := make([]Element, 0, len(orders))
-	for _, name := range orders {
-		coreCoverage, err := strconv.ParseFloat(cores[name], 64)
-		if err != nil {
-			return err
-		}
-
-		firstCoverage, err := strconv.ParseFloat(firsts[name], 64)
-		if err != nil {
-			return err
-		}
-
-		elements = append(elements, Element{
-			Business:      name,
-			CoreCoverage:  coreCoverage,
-			FirstCoverage: firstCoverage,
-			Owner:         owners[name],
-		})
-	}
-
+func WriteReport(w io.Writer, kind string, infos []BusinessInfo) error {
 	var text string
 	switch kind {
 	case "HTML":
 		text = HTML
+	case "XHTML":
+		text = XHTML
 	case "MD":
 		text = MD
+	case "XMD":
+		text = XMD
 	default:
 		text = MD
 	}
@@ -147,5 +242,5 @@ func WriteReport(w io.Writer, kind string, firsts map[string]string, cores map[s
 	if err != nil {
 		return err
 	}
-	return t.Execute(w, elements)
+	return t.Execute(w, infos)
 }
--- tools/coverage-report/run.sh
+++ tools/coverage-report/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ./coverage-report MD <coverage.data >coverage.md
+./coverage-report XMD <coverage.data >coverage-x.md
 ./coverage-report HTML <coverage.data >coverage.html
+./coverage-report XHTML <coverage.data >coverage-x.html
 
