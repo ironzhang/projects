3a5fb2312927e3832f98f64d5f8ac9d9dea67ae0
zhanghui <iron.zhang@foxmail.com>
2023-09-20 18:46:34 +0800

    update

--- go-lang/studies/kubernetes/informer/main.go
+++ go-lang/studies/kubernetes/informer/main.go
@@ -5,48 +5,56 @@ import (
 	"log"
 
 	v1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
-	"k8s.io/client-go/informers"
+
 	"k8s.io/client-go/kubernetes"
-	corev1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func PrintPods(pods []*v1.Pod) {
-	for _, pod := range pods {
-		fmt.Printf("%s\n", pod.String())
+func PrintPod(pod *v1.Pod) {
+	fmt.Printf("%s.%s\n", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
+}
+
+type PodEventHandler struct {
+	store cache.Store
+}
+
+func (p *PodEventHandler) OnAdd(obj interface{}) {
+	objs := p.store.List()
+	if len(objs) > 0 {
+		log.Printf("on add:")
+		for _, obj := range objs {
+			pod := obj.(*v1.Pod)
+			PrintPod(pod)
+		}
 	}
 }
 
-type Controller struct {
-	lister   corev1.PodLister
-	informer cache.SharedIndexInformer
-}
-
-func NewController(lister corev1.PodLister, informer cache.SharedIndexInformer) *Controller {
-	return &Controller{
-		lister:   lister,
-		informer: informer,
+func (p *PodEventHandler) OnUpdate(oldObj, newObj interface{}) {
+	objs := p.store.List()
+	if len(objs) > 0 {
+		log.Printf("on update:")
+		for _, obj := range objs {
+			pod := obj.(*v1.Pod)
+			PrintPod(pod)
+		}
 	}
 }
 
-func (p *Controller) Run() error {
-	selector := labels.NewSelector()
-	requirement, err := labels.NewRequirement("app", selection.Equals, []string{"nginx"})
-	if err != nil {
-		return err
+func (p *PodEventHandler) OnDelete(obj interface{}) {
+	objs := p.store.List()
+	if len(objs) > 0 {
+		log.Printf("on delete:")
+		for _, obj := range objs {
+			pod := obj.(*v1.Pod)
+			PrintPod(pod)
+		}
 	}
-	selector.Add(*requirement)
-
-	pods, err := p.lister.Pods("dev").List(selector)
-	if err != nil {
-		return err
-	}
-	PrintPods(pods)
-
-	return nil
 }
 
 func main() {
@@ -64,14 +72,24 @@ func main() {
 		return
 	}
 
-	// new informer
-	factory := informers.NewSharedInformerFactory(clientset, 0)
-	podInformer := factory.Core().V1().Pods()
-
-	// run controller
-	controller := NewController(podInformer.Lister(), podInformer.Informer())
-	if err = controller.Run(); err != nil {
-		log.Printf("controller run: %v", err)
+	lselector := labels.NewSelector()
+	r, err := labels.NewRequirement("app", selection.Equals, []string{"nginx"})
+	if err != nil {
+		log.Printf("new requirement: %v", err)
 		return
 	}
+	lselector = lselector.Add(*r)
+	fselector := fields.Everything()
+
+	podEventHandler := &PodEventHandler{}
+	optionsModifier := func(options *metav1.ListOptions) {
+		options.LabelSelector = lselector.String()
+		options.FieldSelector = fselector.String()
+	}
+	podsListWatcher := cache.NewFilteredListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", "dev", optionsModifier)
+	store, controller := cache.NewInformer(podsListWatcher, &v1.Pod{}, 0, podEventHandler)
+	podEventHandler.store = store
+
+	ch := make(chan struct{})
+	controller.Run(ch)
 }
